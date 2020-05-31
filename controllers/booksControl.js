4b849e8cae7361dee6a61a6152604bba09815973
Matthew Bell <matthewbell1030@gmail.com>
2020-05-30 17:36:45 -0700

    Completed controller 'booksControl.js'

--- controllers/booksControl.js
+++ controllers/booksControl.js
@@ -2,6 +2,13 @@ const db = require('../models');
 
 // Methods for the controller
 module.exports = {
+    // CREATE
+    create: function (req, res) {
+        db.Book.create(req.body)
+          .then((dbModel) => res.json(dbModel))
+          .catch((err) => res.status(400).json(err));
+    },
+    // READ
     findAll: (req,res) => {
         db.Book.find(req.query)
             .sort({ date: -1 })
@@ -13,5 +20,17 @@ module.exports = {
             .then((dbModel) => res.json(dbModel))
             .catch((err) => res.status(400).json(err));
     },
-
+    // UPDATE
+    update: function (req, res) {
+        db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+          .then((dbModel) => res.json(dbModel))
+          .catch((err) => res.status(400).json(err));
+    },
+    // DELETE
+    remove: function (req, res) {
+        db.Book.findById({ _id: req.params.id })
+          .then((dbModel) => dbModel.remove())
+          .then((dbModel) => res.json(dbModel))
+          .catch((err) => res.status(400).json(err));
+    }
 }
