02f1c04b514ffffe29c358109724e7415dde033c
Matthew Bell <matthewbell1030@gmail.com>
2020-06-01 01:24:09 -0700

    added 'Container' and 'Col' components.

---
+++ client/src/components/Grid/Container/index.js
@@ -0,0 +1,10 @@
+// Dependencies
+import React from "react";
+
+// Component
+function Container(props) {
+    return <div className = "container">{props.children}</div>;
+};
+
+// Exports
+export default Container;
