c55dd8d100c16d7aea3b8e5ec791c7985ff2d120
Matthew Bell <matthewbell1030@gmail.com>
2020-06-01 00:53:16 -0700

    fourth attempt to corret '.gitignore' file

---
+++ client/build/precache-manifest.6a43be29f10c2aa11dd6c2f018bcb64c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4aa6324316ab271112",
+    "url": "/static/css/main.d2badc19.chunk.css"
+  },
+  {
+    "revision": "be4aa6324316ab271112",
+    "url": "/static/js/main.94e52b0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "c08aacca2af9d02ebd37b58b16e92f95",
+    "url": "/index.html"
+  }
+];
