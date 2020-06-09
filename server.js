2f7506d9396f9750a446f9c2e0852d041c65e75e
Matthew Bell <matthewbell1030@gmail.com>
2020-06-09 12:27:21 -0700

    re-attempting to fix heroku build in server.js

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ const app = express();
 const PORT = process.env.PORT || 4242;
 
 
+// Middleware
+app.use(
+    express.urlencoded({
+        extended: true 
+    })
+);
+app.use(
+    express.json()
+);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+
+// Routes - front-end / API 
+app.use(routes);
+
+
 // Database connection / configuration
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/google-books-search";
 
@@ -22,20 +39,6 @@ mongoose.connect(
   .then(() => console.log("Connected to MongoDB Successfully!"))
   .catch(err => console.log(err));
 
-
-// Middleware
-app.use(
-    express.urlencoded({
-        extended: true 
-    })
-);
-app.use(
-    express.json()
-);
-
-
-// Routes - front-end / API 
-app.use(routes);
 
 // Start the back-end server
 app.listen(PORT, () => {
